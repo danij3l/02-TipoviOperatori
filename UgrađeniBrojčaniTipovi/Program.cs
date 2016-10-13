021929b28f5cb5f77a2c0fb8f323d6398279a42b
Danijel Pavić <danijelpavic21@gmail.com>
2016-10-14 00:20:57 +0200

    Igranje sa UgrađenimBrojčanimTipovima.

--- UgrađeniBrojčaniTipovi/Program.cs
+++ UgrađeniBrojčaniTipovi/Program.cs
@@ -7,8 +7,19 @@ namespace CSharp.UgrađeniBrojčaniTipovi
         static void Main(string[] args)
         {
             Console.WriteLine(int.MaxValue);
-
-
+            Console.WriteLine(int.MinValue);
+            int x = int.MaxValue;
+            x = x + 1;
+            Console.WriteLine(x); 
+            int y = int.MinValue;
+            y = y - 1;
+            Console.WriteLine(y);
+            Console.WriteLine(double.MaxValue);
+            Console.WriteLine(double.MinValue);
+            //Console.WriteLine(1/0); nije dozvoljeno dijeljenje s nulom
+            //Console.WriteLine(-1/0); nije dozvoljeno dijeljenje s nulom
+            double d = 0;
+            Console.WriteLine(d/0); //NaN
 
             Console.ReadKey();
         }
