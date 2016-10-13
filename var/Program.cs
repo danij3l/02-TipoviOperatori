744f3c5e64dd11b9f1a242550c60e4ff1bca4240
Danijel Pavić <danijelpavic21@gmail.com>
2016-10-14 00:58:15 +0200

    Var

--- var/Program.cs
+++ var/Program.cs
@@ -11,7 +11,17 @@ namespace CSharp.TipoviOperatori
     {
         static void Main(string[] args)
         {
+            var i0 = 1;
+            var d0 = 1.1111;
+            //i0 = d0; ->ne
 
+            int i1 = i0;
+            double d1 = d0;
+
+            var s = new S();
+            s.a = 1410;
+            s.b = 14.10;
+             
         }
     }
 }
